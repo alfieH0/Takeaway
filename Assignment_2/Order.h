af1d4f1d6a3525795da705b7d11e96628ac7f93f
alfieH0 <Alfieholman@gmail.com>
2024-01-10 19:45:35 +0000

    comments needed

--- Assignment_2/Order.h
+++ Assignment_2/Order.h
@@ -13,7 +13,7 @@ private:
     double total;
     double savings;
     string formatTo2DP(double value) const;
-
+    void calculateDiscount();
 public:
     Order();
     ~Order();
@@ -22,6 +22,6 @@ public:
     string toString() const override;
     void add(shared_ptr<Item> item);
     void remove(size_t position);
-    void calculateDiscount();
+  
 };
 #endif
